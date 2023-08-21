ce6568b368a9fcae8d8a281eb246c87746e04c75
Bjverde <bjverde@yahoo.com.br>
2023-08-20 22:22:31 -0300

    :hammer: #1 primeira versão do script

---
+++ documents/script01.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS contato;
+
+CREATE TABLE contato (
+	`id_contato` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`nome`	TEXT NOT NULL,
+	`ddi`	VARCHAR(4) NOT NULL DEFAULT '+55',
+	`ddd`	VARCHAR(2) NOT NULL,
+	`celular`	VARCHAR(9) NOT NULL,
+	`st_whatsapp`	VARCHAR(2) DEFAULT 'S',
+	`avisado`	VARCHAR(2) DEFAULT 'N'
+);
+
+DROP TABLE IF EXISTS mensagem;
+
+CREATE TABLE mensagem (
+	`id_mensagem`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`mensagem`	TEXT NOT NULL
+);
