cf63aeacd10835ffcb73adb81d3baca1bb49c42b
Bjverde <bjverde@yahoo.com.br>
2023-08-21 22:01:12 -0300

    :hammer: #3 comentando voltar

--- system/app/control/whats/views/MensagemForm.class.php
+++ system/app/control/whats/views/MensagemForm.class.php
@@ -47,7 +47,7 @@ class MensagemForm extends TPage
         //primeiro em ingles e depois traduzindo
         $frm->setAction( _t('Save'), 'onSave', null, 'fa:save', 'green' );
         $frm->setActionLink( _t('Clear'), 'onClear', null, 'fa:eraser', 'red');
-        $frm->setActionLink( _t('Back'), ['MensagemFormList','onReload'], null, 'fas:arrow-left', '#000000');
+        //$frm->setActionLink( _t('Back'), ['MensagemFormList','onReload'], null, 'fas:arrow-left', '#000000');
 
         $this->form = $frm->show();
         $this->form->setData( TSession::getValue(__CLASS__.'_filter_data'));
